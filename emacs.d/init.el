b6326a5b0d6d54acd86c7ab8317a071894e05070
kyle <kpgwinnup@gmail.com>
2020-06-25 21:37:30 -0400

    emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -175,6 +175,8 @@
       (scroll-bar-mode -1)))
 
 (tool-bar-mode -1)
+(menu-bar-mode -1)
+(xterm-mouse-mode 1)
 (fset 'yes-or-no-p 'y-or-n-p)
 (set-face-attribute 'default nil
                     :family "mononoki"
