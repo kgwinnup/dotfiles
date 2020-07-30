c0e524c801d8067b05a45a0893b005427c624356
kgwinnup <kgwinnup@vmware.com>
2020-07-30 11:37:32 -0400

    emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -22,6 +22,33 @@
   (package-install 'use-package))
 (require 'use-package)
 
+(defun my-window-toggle ()
+  "Switch window split from horizontally to vertically, or vice versa.
+i.e. change right window to bottom, or change bottom window to right."
+  (interactive)
+  (require 'windmove)
+  (let ((done))
+    (dolist (dirs '((right . down) (down . right)))
+      (unless done
+        (let* ((win (selected-window))
+               (nextdir (car dirs))
+               (neighbour-dir (cdr dirs))
+               (next-win (windmove-find-other-window nextdir win))
+               (neighbour1 (windmove-find-other-window neighbour-dir win))
+               (neighbour2 (if next-win (with-selected-window next-win
+                                          (windmove-find-other-window neighbour-dir next-win)))))
+          ;;(message "win: %s\nnext-win: %s\nneighbour1: %s\nneighbour2:%s" win next-win neighbour1 neighbour2)
+          (setq done (and (eq neighbour1 neighbour2)
+                          (not (eq (minibuffer-window) next-win))))
+          (if done
+              (let* ((other-buf (window-buffer next-win)))
+                (delete-window next-win)
+                (if (eq nextdir 'right)
+                    (split-window-vertically)
+                  (split-window-horizontally))
+                (set-window-buffer (windmove-find-other-window neighbour-dir) other-buf))))))))
+
+
 (defun my-send-to-shell (cmd)
   "sends a command to the buffer containing an active shell"
   (interactive)
@@ -251,6 +278,7 @@
                "n p" 'previous-buffer
                "n o" 'delete-other-windows
                "n d" 'kill-buffer-and-window
+               "n l" 'my-window-toggle
                ;; magit
                "m s" 'magit
                "m e" 'mu4e
