94f3e42341879f1b7c71ee115191cc3b9dfd4437
Kyle Gwinnup <kgwinnup@vmware.com>
2022-03-17 20:56:13 -0400

    adds send buffer to tmux functions

--- init.lua
+++ init.lua
@@ -5,15 +5,16 @@ require('packer').startup(function()
   use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
   use 'morhetz/gruvbox'
   use 'ervandew/supertab'
+  use 'scrooloose/nerdtree'
+  use 'rust-lang/rust.vim'
 end)
 
--- vim.opt.backupdir = ~/.config/nvim/backups
--- vim.opt.dir = ~/.config/nvim/backups/
-vim.mapleader = " "
+-- vim.fn.system("mkdir -p ~/.config/nvim/backups")
+-- vim.opt.backupdir = vim.env.HOME .. "/.config/nvim/backups"
+-- vim.opt.dir = vim.env.HOME .. "/.config/nvim/backups/"
 
+vim.mapleader = " "
 vim.g.mapleader = " "
-vim.g.netrw_winsize = 20
-vim.g.SuperTabDefaultCompletionType = "<c-x><c-o>"
 
 vim.opt.timeoutlen = 2000
 vim.opt.shortmess = "at"
@@ -37,31 +38,65 @@ vim.opt.background = "dark"
 vim.cmd("colorscheme gruvbox")
 vim.opt.colorcolumn = "0"
 
-vim.api.nvim_set_keymap("n", "<leader>np", ":bprevious<cr>", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>nn", ":bnext<cr>", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>nd", ":bdelete<cr>", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>no", ":only<cr>", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>ns", "<C-W><C-W>", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>j", "<C-d>", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>k", "<C-u>", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>=", "<C-w>=", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>-", ":res -5<cr>", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>+", ":res +5<cr>", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>nt", ":Lexplore<cr>", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>ss", ":setlocal spell spelllang=en_us<cr>", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>sf", ":setlocal nospell", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>sn", "]s", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>sp", "[s", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>sr", "z=", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>sa", "zg", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>vp", ":lua tmux_send_command('')<cr>", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>vl", ":lua tmux_send_last_command()<cr>", { noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>mb", ":lua tmux_send_command('git blame -L ' .. vim.fn.line('.') .. ',' .. vim.fn.line('.') .. ' ' .. vim.fn.expand('%:p'))<cr>", { noremap = true})
+local opts = { noremap=true, silent=true }
+
+vim.api.nvim_set_keymap("n", "<leader>np", ":bprevious<cr>", opts)
+vim.api.nvim_set_keymap("n", "<leader>nn", ":bnext<cr>", opts)
+vim.api.nvim_set_keymap("n", "<leader>nd", ":bdelete<cr>", opts)
+vim.api.nvim_set_keymap("n", "<leader>no", ":only<cr>", opts)
+vim.api.nvim_set_keymap("n", "<leader>ns", "<C-W><C-W>", opts)
+vim.api.nvim_set_keymap("n", "<leader>j", "<C-d>", opts)
+vim.api.nvim_set_keymap("n", "<leader>k", "<C-u>", opts)
+vim.api.nvim_set_keymap("n", "<leader>=", "<C-w>=", opts)
+vim.api.nvim_set_keymap("n", "<leader>-", ":res -5<cr>", opts)
+vim.api.nvim_set_keymap("n", "<leader>+", ":res +5<cr>", opts)
+vim.api.nvim_set_keymap("n", "<leader>nt", ":NERDTreeToggle<cr>", opts)
+vim.api.nvim_set_keymap("n", "<leader>ss", ":setlocal spell spelllang=en_us<cr>", opts)
+vim.api.nvim_set_keymap("n", "<leader>sf", ":setlocal nospell", opts)
+vim.api.nvim_set_keymap("n", "<leader>sn", "]s", opts)
+vim.api.nvim_set_keymap("n", "<leader>sp", "[s", opts)
+vim.api.nvim_set_keymap("n", "<leader>sr", "z=", opts)
+vim.api.nvim_set_keymap("n", "<leader>sa", "zg", opts)
+vim.api.nvim_set_keymap("n", "<leader>rr", ":lua tmux_send_buf()<cr>", opts)
+vim.api.nvim_set_keymap("n", "<leader>vp", ":lua tmux_send_command('')<cr>", opts)
+vim.api.nvim_set_keymap("n", "<leader>vl", ":lua tmux_send_last_command()<cr>", opts)
+vim.api.nvim_set_keymap("n", "<leader>mb", ":lua tmux_send_command('git blame -L ' .. vim.fn.line('.') .. ',' .. vim.fn.line('.') .. ' ' .. vim.fn.expand('%:p'))<cr>", opts)
+vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+
+
+-- supertab for lsp tab completion
+vim.g.SuperTabDefaultCompletionType = "<c-x><c-o>"
+
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>t', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gg', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gp', ':pop<cr>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gl', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gr', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gh', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+end
+
+local servers = { 'rust_analyzer', 'clangd' }
+for _, lsp in pairs(servers) do
+  require('lspconfig')[lsp].setup {
+    on_attach = on_attach,
+    flags = {
+      -- This will be the default in neovim 0.7+
+      debounce_text_changes = 150,
+    }
+  }
+end
+
+-- nerd tree settings
+vim.g.NERDTreeQuitOnOpen = 1
 
 -- rust specific configs
 vim.g.rust_recommended_style = 1
 vim.g.rustfmt_autosave = 1
-vim.g.rustfmt_command = 'rustfmt --force'
 
 -- tmux operations
 vim.g.tmux_last_command = ""
@@ -87,8 +122,7 @@ function tmux_send_command(txt)
 
     if txt == "" then
         input = vim.fn.input("tmux input: ")
-        if input == "" then
-        else
+        if input ~= "" then
             tmux_send_command(input)
         end 
     else
@@ -105,35 +139,40 @@ function tmux_send_last_command()
     tmux_send_command(vim.g.tmux_last_command) 
 end
 
-local opts = { noremap=true, silent=true }
-
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-  
-  vim.cmd("autocmd BufWritePre *.rs lua vim.lsp.buf.formatting_sync(nil, 1000)")
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>t', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gg', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gp', ':pop<cr>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gl', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gh', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+local function tmux_send_block() 
+    start = vim.fn.line(".")
+    _end = vim.fn.search("^$")
+    lines = vim.fn.getbufline(vim.fn.bufnr("%"), start, _end-1)
+    for k,v in pairs(lines) do
+        tmux_send_command(v)
+    end
+    vim.fn.execute("normal! :" .. _end)
 end
 
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
-local servers = { 'rust_analyzer', 'clangd' }
-for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
-    on_attach = on_attach,
-    flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
-    }
-  }
+local function tmux_send_function() 
+    start = vim.fn.line(".")
+    vim.fn.execute("normal! ][")
+    _end = vim.fn.line(".")
+    lines = vim.fn.getbufline(vim.fn.bufnr("%"), start, _end)
+    for k,v in pairs(lines) do
+        if v == "" then
+            tmux_send_command("\n")
+        else 
+            tmux_send_command(v)
+        end
+    end
+    vim.fn.execute("normal! :" .. _end)
 end
+
+function tmux_send_buf()
+    if string.find(vim.fn.getline("."), "function") ~= nil then
+        tmux_send_function()
+    else
+        tmux_send_block()
+    end
+end
+
+
+
+
 
