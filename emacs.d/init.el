069917e7018f8e671e32c4f8732a3ccc3488e4c8
Kyle Gwinnup <kpgwinnup@gmail.com>
2022-10-22 09:08:18 -0400

    resolved conflicts

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -23,10 +23,6 @@
           "~/.local/bin"
           "~/.cargo/bin"))
 
-(add-to-list 'load-path "/Users/kylegwinnup/.emacs.d/asdf.el")
-(load "~/.emacs.d/asdf.el")
-(asdf-enable)
-
 ; make modeline less noisy
 (setq-default mode-line-format
               '("%l:%c "
@@ -40,6 +36,7 @@
               comp-async-report-warnings-errors nil
               compilation-scroll-output t
               scroll-step 1
+              gc-cons-threshold 100000000
               scroll-conservatively  10000
               mouse-wheel-scroll-amount '(1 ((shift) . 1))
               mouse-wheel-progressive-speed nil
@@ -99,6 +96,11 @@
 
 (use-package vterm
   :ensure t)
+
+(use-package langtool
+  :ensure t
+  :config
+  (setq langtool-language-tool-jar "/usr/local/Cellar/languagetool/5.9/libexec/languagetool-commandline.jar"))
 
 (use-package eldoc-box
   :ensure t
@@ -359,6 +361,99 @@
                       :height (+ size kg/font-size))
   (setq kg/font-size (+ size kg/font-size)))
 
+(defun kg/magit-show-with-difftastic (rev)
+  "Show the result of \"git show REV\" with GIT_EXTERNAL_DIFF=difft."
+  (interactive
+   (list (or
+          ;; If REV is given, just use it.
+          (when (boundp 'rev) rev)
+          ;; If not invoked with prefix arg, try to guess the REV from
+          ;; point's position.
+          (and (not current-prefix-arg)
+               (or (magit-thing-at-point 'git-revision t)
+                   (magit-branch-or-commit-at-point)))
+          ;; Otherwise, query the user.
+          (magit-read-branch-or-commit "Revision"))))
+  (if (not rev)
+      (error "No revision specified")
+    (kg/magit--with-difftastic
+     (get-buffer-create (concat "*git show difftastic " rev "*"))
+     (list "git" "--no-pager" "show" "--ext-diff" rev))))
+
+(defun kg/magit--with-difftastic (buffer command)
+  "Run COMMAND with GIT_EXTERNAL_DIFF=difft then show result in BUFFER."
+  (let ((process-environment
+         (cons (concat "GIT_EXTERNAL_DIFF=difft --width="
+                       (number-to-string (frame-width)))
+               process-environment)))
+    ;; Clear the result buffer (we might regenerate a diff, e.g., for
+    ;; the current changes in our working directory).
+    (with-current-buffer buffer
+      (setq buffer-read-only nil)
+      (erase-buffer))
+    ;; Now spawn a process calling the git COMMAND.
+    (make-process
+     :name (buffer-name buffer)
+     :buffer buffer
+     :command command
+     ;; Don't query for running processes when emacs is quit.
+     :noquery t
+     ;; Show the result buffer once the process has finished.
+     :sentinel (lambda (proc event)
+                 (when (eq (process-status proc) 'exit)
+                   (with-current-buffer (process-buffer proc)
+                     (goto-char (point-min))
+                     (ansi-color-apply-on-region (point-min) (point-max))
+                     (setq buffer-read-only t)
+                     (view-mode)
+                     (end-of-line)
+                     ;; difftastic diffs are usually 2-column side-by-side,
+                     ;; so ensure our window is wide enough.
+                     (let ((width (current-column)))
+                       (while (zerop (forward-line 1))
+                         (end-of-line)
+                         (setq width (max (current-column) width)))
+                       ;; Add column size of fringes
+                       (setq width (+ width
+                                      (fringe-columns 'left)
+                                      (fringe-columns 'right)))
+                       (goto-char (point-min))
+                       (pop-to-buffer
+                        (current-buffer)
+                        `(;; If the buffer is that wide that splitting the frame in
+                          ;; two side-by-side windows would result in less than
+                          ;; 80 columns left, ensure it's shown at the bottom.
+                          ,(when (> 80 (- (frame-width) width))
+                             #'display-buffer-at-bottom)
+                          (window-width
+                           . ,(min width (frame-width))))))))))))
+
+(defun kg/magit-diff-with-difftastic (arg)
+  "Show the result of \"git diff ARG\" with GIT_EXTERNAL_DIFF=difft."
+  (interactive
+   (list (or
+          ;; If RANGE is given, just use it.
+          (when (boundp 'range) range)
+          ;; If prefix arg is given, query the user.
+          (and current-prefix-arg
+               (magit-diff-read-range-or-commit "Range"))
+          ;; Otherwise, auto-guess based on position of point, e.g., based on
+          ;; if we are in the Staged or Unstaged section.
+          (pcase (magit-diff--dwim)
+            ('unmerged (error "unmerged is not yet implemented"))
+            ('unstaged nil)
+            ('staged "--cached")
+            (`(stash . ,value) (error "stash is not yet implemented"))
+            (`(commit . ,value) (format "%s^..%s" value value))
+            ((and range (pred stringp)) range)
+            (_ (magit-diff-read-range-or-commit "Range/Commit"))))))
+  (let ((name (concat "*git diff difftastic"
+                      (if arg (concat " " arg) "")
+                      "*")))
+    (kg/magit--with-difftastic
+     (get-buffer-create name)
+     `("git" "--no-pager" "diff" "--ext-diff" ,@(when arg (list arg))))))
+
 (use-package dired-sidebar
   :ensure t
   :init
@@ -368,6 +463,17 @@
               (display-line-numbers-mode -1)
               (unless (file-remote-p default-directory)
                 (auto-revert-mode)))))
+
+(transient-define-prefix kg/magit-aux-commands ()
+  "My personal auxiliary magit commands."
+  ["Auxiliary commands"
+   ("d" "Difftastic Diff (dwim)" kg/magit-diff-with-difftastic)
+   ("s" "Difftastic Show" kg/magit-show-with-difftastic)])
+
+(transient-append-suffix 'magit-dispatch "!"
+  '("#" "My Magit Cmds" kg/magit-aux-commands))
+
+(define-key magit-status-mode-map (kbd "#") #'kg/magit-aux-commands)
 
 (use-package bind-map
   :ensure t
